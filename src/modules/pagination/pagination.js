79370fbb9ed0bc1d77beda74427e7443c3144d3a
Lowech <78932817+Lowech@users.noreply.github.com>
2021-07-28 13:08:56 +0300

    вносил правки в стиляхдобиваясь отзывчивости

--- src/modules/pagination/pagination.js
+++ src/modules/pagination/pagination.js
@@ -3,6 +3,26 @@ import  $ from 'jquery'// подключение к файлу библиоте�
 console.log("ffff")
 
 
+
+function simpleTemplating(data) {
+  var html = '<ul>';
+  $.each(data, function(index, item){
+      html += '<li>'+ item +'</li>';
+  });
+  html += '</ul>';
+  return html;
+}
+$('#pagination-container').pagination({
+  dataSource: ['1',' 2',' 3', '4', '5', '6', '7',' ...' , '195'],
+  pageSize: 5,
+  autoHidePrevious: true,
+  autoHideNext: true,
+  callback: function(data, pagination) {
+      // template method of yourself
+      var html = simpleTemplating(data);
+      $('#data-container').html(html);
+  }
+})
 /*let coloredDlock =  document.querySelector('.js-number');
   let coloredItem =  document.querySelector('.js-number__item');
 $(".js-number_color-green").on('click',sliderMovementButton);
