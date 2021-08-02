458777ef0a80a9d9befb7204352bdf6c07702a37
Lowech <78932817+Lowech@users.noreply.github.com>
2021-08-02 13:42:11 +0300

    делал страницу room details и ее элементы resposive, написал функцию добавления и удаления лайков

--- src/modules/impressions-room/impressions-room.js
+++ src/modules/impressions-room/impressions-room.js
@@ -10,8 +10,8 @@ let grd1= ctx.createLinearGradient(80,0,0,160);
 grd1.addColorStop(0, '#FFE39C');
 grd1.addColorStop(1, '#FFBA9C');
 ctx.strokeStyle = grd1;
-ctx.lineWidth = 4;
-ctx.arc(75,77,68,1.5,(Math.PI/180)*270); 
+ctx.lineWidth = 5;
+ctx.arc(75,77,68,1.5,(Math.PI/180)*271); 
 ctx.stroke();
 //правая нижняя дуга
 ctx.beginPath();
@@ -19,8 +19,8 @@ let grd2= ctx.createLinearGradient(80,0,0,160);
 grd2.addColorStop(0, '#6FCF97');
 grd2.addColorStop(1, '#66D2EA');
 ctx.strokeStyle = grd2;
-ctx.lineWidth = 4;
-ctx.arc(83,77,68,0.07,(Math.PI/180)*90); 
+ctx.lineWidth = 5;
+ctx.arc(83,77,68,0.04,(Math.PI/180)*90); 
 ctx.stroke();
 //правая верхняя дуга
 ctx.beginPath();
@@ -28,8 +28,8 @@ let grd3= ctx.createLinearGradient(80,0,0,160);
 grd3.addColorStop(0, '#BC9CFF');
 grd3.addColorStop(1, '#8BA4F9');
 ctx.strokeStyle = grd3;
-ctx.lineWidth = 4;
-ctx.arc(80,79,71,4.7,(Math.PI/180)*360); 
+ctx.lineWidth = 5;
+ctx.arc(80,79,71,4.7,(Math.PI/181)*360); 
 ctx.stroke();
 console.log(Math.PI/180*360)
 }
