871d74e0a1103f535e50339c91cf319ccd8ad0b2
Lowech <78932817+Lowech@users.noreply.github.com>
2021-08-09 00:05:27 +0300

    привожу в порядок файлы и папки удаляя ненужные и меняю названия при необходимости, подключил полностью календарь, кроме позиционирования, полностью закончил страницу ui-kit, по мере проверки страниц и файлов вношу мелкие правки по вертске

--- src/modules/filter-date-dropdown-input/filter-date-dropdown.js
+++ src/modules/filter-date-dropdown-input/filter-date-dropdown.js
@@ -1,6 +1,34 @@
 import  $ from 'jquery'// подключение к файлу библиотеки jquery
+import 'air-datepicker/dist/js/datepicker.min.js'//plugins calendar
+$(".datepicker-here").datepicker({
+    language: {
+        clear: 'очистить',
+        today: 'применить'
+    },
+    dateFormat:"d M",
+    offset: 10,
+    range: true, 
+    multipleDatesSeparator: " - ",
+    multipleDates: true,
+    clearButton: true,
+    todayButton: true,
+    navTitles: {
+        days: ' MM yyyy'
+    },
+    prevHtml: '<img src="src/modules/calendar/img/arrow_back.png" alt="img">',
+    nextHtml: '<img src="src/modules/calendar/img/arrow_forward.png" alt="img">',
 
-$('.js-filter-date-dropdown').on('click',showTable);
+    onSelect:function (fd, date){
+        $(".js-filter-date-dropdown__input").val(fd.toLowerCase());
+    }
+    
+});
+document.addEventListener("click",function(event){
+    if(event.target.dataset.action=="today"){
+        $(".js-filter-date-dropdown__input").blur();
+    }
+});
+/*$('.js-filter-date-dropdown').on('click',showTable);
 function showTable(){
     if(this.classList.contains('filter-date-dropdown_active')){
         $('.js-filter-date-dropdown').removeClass('filter-date-dropdown_active');
@@ -18,4 +46,4 @@ function datepickerShow(){
     if(cls){
         $('.datepicker-here').data('datepicker').show();
     }
-}
+}*/
